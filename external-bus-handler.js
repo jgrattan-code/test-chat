70a83c99d3e49797114d6fa30f1ca3b1fb198c9f
Alexander Grattan <alex@grattan.me>
2021-04-17 15:02:36 -0400

    modified:   external-bus-handler.js

--- external-bus-handler.js
+++ external-bus-handler.js
@@ -2,9 +2,12 @@
  (function(root) {
      root.__8x8Chat = {
          onInit: function(bus) {
-  bus.publish("chat:set-system-messages", {
-      agentTyping: "(modified) Agent is typing..."
-});
+bus.publish("chat:disable-proxy");
+bus.subscribe("customer:message-received", function(data) {
+console.log(data.message);
+      // data.message contains the customer message, and data.id
+      // the message ID to be sent back when proxying
+  });
 
          }
      };
