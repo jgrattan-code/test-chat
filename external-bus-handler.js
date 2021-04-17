cf3dcb810dc72a942b478b3404ea02e18235949d
Alexander Grattan <alex@grattan.me>
2021-04-17 14:54:24 -0400

    modified:   external-bus-handler.js

--- external-bus-handler.js
+++ external-bus-handler.js
@@ -2,7 +2,7 @@
  (function(root) {
      root.__8x8Chat = {
          onInit: function(bus) {
-  message_bus.publish("chat:set-system-messages", {
+  bus.publish("chat:set-system-messages", {
       agentTyping: "(modified) Agent is typing..."
 });
 
